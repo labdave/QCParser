ba89d54b4b761b7d546afd4465f80d5af2b40448
Tushar Dave <tushar.dave@duke.edu>
2019-01-09 15:52:44 -0500

    Modify TNADemuxReport module
    Now, the module updated for new demux stat report file.

--- QCModules/Parsers/TNADemuxReport.py
+++ QCModules/Parsers/TNADemuxReport.py
@@ -4,7 +4,7 @@ from QCModules.BaseModule import QCParseException
 class TNADemuxReport(BaseParser):
 
     DESCRIPTION     = "Parse RNA/DNA report from TNA demux script"
-    INPUT_FILE_DESC = "CSV output from TNA demux script (adapter_stats.csv)"
+    INPUT_FILE_DESC = "CSV output from TNA demux script (all_barcode_stats.csv)"
 
     def __init__(self, sys_args):
         super(TNADemuxReport, self).__init__(sys_args)
@@ -25,20 +25,20 @@ class TNADemuxReport(BaseParser):
                 if first_line:
                     if "total_percent_rna" not in line:
                         raise QCParseException("TNADemux file '%s' does not contain 'total_percent_rna' in first line. "
-                                           "Make sure it's "
-                                           "the adapter_stats.txt!")
-                    elif len(line) != 4:
+                                           "Make sure it's the all_barcode_stats.txt!")
+                    elif len(line) != 8:
                         raise QCParseException("TNADemux file '%s' does not have exactly four columns! Expected\n"
-                                               "columns: sample, read1_percent, read2_percent, total_percent_rna\n"
-                                               "Make sure it's "
-                                               "the adapter_stats.txt!")
+                                               "columns: sample,barcode,read1_percent,read2_percent,",
+                                               "total_percent_rna,total_reads,rna_reads,nonrna_reads\n"
+                                               "Make sure it's the all_barcode_stats.txt!")
                     first_line = False
                 else:
-                    self.add_entry("Total_RNA_Percent", float(line[3]))
-                    self.add_entry("R1_Percent", float(line[1]))
-                    self.add_entry("R2_Percent", float(line[2]))
+                    self.add_entry("R1_Percent", float(line[2]), note = line[1])
+                    self.add_entry("R2_Percent", float(line[3]), note = line[1])
+                    self.add_entry("Total_RNA_Percent", float(line[4]), note = line[1])
+                    self.add_entry("Total_reads", int(float(line[5])), note = line[1])
+                    self.add_entry("RNA_reads", int(float(line[6])), note = line[1])
+                    self.add_entry("NON_RNA_reads", int(float(line[7])), note = line[1])
 
     def define_required_colnames(self):
-        return ["R1_Percent", "R2_Percent", "Total_RNA_Percent"]
-
-
+        return ["R1_Percent", "R2_Percent", "Total_RNA_Percent", "Total_reads", "RNA_reads", "NON_RNA_reads"]
